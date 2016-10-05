9cf1fdb1b9f0f31483906ab40af0c131318a46f9
fiddlerwoaroof <fiddlerwoaroof@gmail.com>
2016-10-05 14:14:43 -0700

    Add a screenshot pulling command

--- slack-client.lisp
+++ slack-client.lisp
@@ -85,28 +85,44 @@
 	      (yason:parse message :object-as object-as))
 	    message-p)))
 
+(defparameter *ignored-messages* '(:pong))
+(defgeneric handle-message (type event-pump ts channel message)
+  (:method :before (type event-pump ts channel message)
+    (declare (ignore event-pump ts channel))
+    (unless (member type *ignored-messages*)
+      (fresh-line)
+      (yason:encode message)
+      (terpri)))
+  (:method (type (event-pump event-pump) ts channel message)
+    (declare (ignore type event-pump ts channel message))
+    nil)
+  (:method ((type (eql :pong)) (event-pump event-pump) ts channel message)
+    (declare (ignore ts type channel))
+    (decf (waiting-pings event-pump))
+    (format t "~&pong received for ~a"
+	    (gethash "reply_to" message))))
+    
 (defun event-loop (event-pump)
   (loop with message with message-p
      do (multiple-value-setq (message message-p) (get-event event-pump))
      when message-p do
        (let ((type (gethash "type" message))
-	     (reply (gethash "reply_to" message)))
+	     (reply (gethash "reply_to" message))
+	     (ts (gethash "ts" message))
+	     (channel (gethash "channel" message)))
 	 (cond (type (string-case (gethash "type" message)
 		       ("message" (format t "~&MSG: <~a/~a> ~a~%"
 					  (gethash "channel" message)
 					  (gethash "user" message)
 					  (gethash "text" message))
 				  (when-let* ((msg (gethash "text" message))
-					      (parsed-message (tokens msg))
-					      (ts (gethash "ts" message))
-					      (channel (gethash "channel" message))) 
+					      (parsed-message (tokens msg))) 
 				    (when (eql #\; (elt msg 0))
 				      (handle-command event-pump ts channel
 						      (car parsed-message)
 						      (cdr parsed-message)))))
-		       (t (fresh-line)
-			  (yason:encode message)
-			  (terpri))))
+		       (t (handle-message (make-keyword (string-upcase type))
+					  event-pump ts channel message))))
 	       (reply )))
        do (sleep 0.01)))
 
@@ -144,6 +160,7 @@
     (when data
       (aconsf message "data" data))
 
+    (format t "~&pinging with id: ~a at time: ~a~%" id (local-time:now))
     (incf (waiting-pings client))
     (alist-hash-table message
                       :test 'equal)))
@@ -208,14 +225,14 @@
 						    :content (quri:url-encode-params
 							      (loop for (key value) on args by #'cddr
 								 collect (cons (string-downcase key) value)))
-						    :proxy (list "127.0.0.1" 8080))))))
+						    )))))
 	     ;todo error handling . . .
 	     (resolve api-result)) 
 	 (t (c) (reject c)))))))
 
-(defgeneric api-call (name args)
-  (:method ((name symbol) (args list))
-    (slack-api-call ,)))
+;; (defgeneric api-call (name args)
+;;   (:method ((name symbol) (args list))
+;;     (slack-api-call name)))
 
 (defmacro define-api-wrapper (name required-args &rest args)
   (flet ((name-case (string)
